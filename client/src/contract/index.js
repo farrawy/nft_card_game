ec33678baf5d74f2c46cceb85b6d6f19de9db72b
Ahmed <ahmed.e.alfarra@gmail.com>
2022-11-04 15:42:27 +0300

    first commit

---
+++ client/src/contract/index.js
@@ -0,0 +1,5 @@
+import contract from "./ImmortalDemons.json";
+
+export const ADDRESS = "0xc397c7feC50fCb8c2Ef8910B7229f991B24413Bd";
+
+export const { abi: ABI } = contract;
---
+++ client/src/page/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { PageHOC, CustomInput, CustomButton } from "../components";
+import { useGlobalContext } from "../context/Context";
+
+const Home = () => {
+  const { contract, walletAddress, setShowAlert } = useGlobalContext();
+  const [playerName, setPlayerName] = useState("");
+
+  const handleClick = async () => {
+    try {
+      console.log({ contract });
+      const playerExists = await contract.isPlayer(walletAddress);
+
+      if (!playerExists) {
+        await contract.registerPlayer(playerName, playerName);
+
+        setShowAlert({
+          status: true,
+          type: "info",
+          message: `${playerName} is being summoned!`,
+        });
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <CustomInput
+        label="Name"
+        placeholder="Enter your player name"
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+
+      <CustomButton
+        title="Register"
+        handleClick={handleClick}
+        restStyles="mt-6"
+      />
+    </div>
+  );
+};
+
+export default PageHOC(
+  Home,
+  <>
+    Welcome to Immortal Demons <br /> a Web3 NFT Card Game
+  </>,
+  <>
+    Connect your wallet to start playing <br /> the ultimate Web3 Battle Card
+    Game
+  </>
+);
